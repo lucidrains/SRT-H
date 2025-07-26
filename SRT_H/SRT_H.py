7e986917ba2b31c73fae5c140287241eed993466
lucidrains <lucidrains@gmail.com>
2025-07-26 08:09:07 -0700

    complete the basic ACT

--- SRT_H/SRT_H.py
+++ SRT_H/SRT_H.py
@@ -26,9 +26,10 @@ class ACT(Module):
         *,
         dim_joint_state,
         action_chunk_len,
+        dim_action = 20,
         dim_head = 64,
         heads = 8,
-        vae_encoder_depth = 6,
+        vae_encoder_depth = 3,
         encoder_depth = 6,
         decoder_depth = 6,
         vae_encoder_kwargs: dict = dict(),
@@ -39,9 +40,12 @@ class ACT(Module):
     ):
         super().__init__()
 
+        self.dim = dim
+
         # projections
 
         self.joint_to_token = nn.Linear(dim_joint_state, dim)
+        self.action_to_vae_tokens = nn.Linear(dim_action, dim)
 
         # for the cvae and style vector
 
@@ -52,9 +56,10 @@ class ACT(Module):
             attn_dim_head = dim_head,
         )
 
+        self.attn_pooler = Attention(dim = dim, heads = heads, dim_head = dim_head)
         self.attn_pool_query = Parameter(torch.randn(dim)) # there is evidence attention pooling is better than CLS / global average pooling
 
-        self.to_mean_log_variance = Sequential(
+        self.to_style_vector_mean_log_variance = Sequential(
             Linear(dim, dim * 2, bias = False),
             Rearrange('... (d mean_log_var) -> mean_log_var ... d', mean_log_var = 2)
         )
@@ -68,8 +73,7 @@ class ACT(Module):
             attn_dim_head = dim_head,
         )
 
-        self.action_queries = Parameter(torch.randn(dim) * 1e-2)
-        self.action_pos_emb = Parameter(torch.randn(action_chunk_len, dim))
+        self.action_queries = Parameter(torch.randn(action_chunk_len, dim) * 1e-2)
 
         self.decoder = Encoder(
             dim = dim,
@@ -79,6 +83,8 @@ class ACT(Module):
             cross_attend = True
         )
 
+        self.decoder_embed_to_actions = nn.Linear(dim, dim_action)
+
         # loss related
 
         self.action_loss_fn = action_loss_fn
@@ -86,13 +92,100 @@ class ACT(Module):
 
     def forward(
         self,
-        state,
-        joint_state,
-        actions = None
+        state_tokens,        # (b n d)
+        joint_state,         # (d)
+        actions = None,      # (b na da)
+        style_token = None,  # (d) | (b d)
+        return_loss_breakdown = False
     ):
-        is_sampling = not exists(actions)
 
-        raise NotImplementedError
+        # variables
+
+        batch, device = state_tokens.shape[0], state_tokens.device
+
+        is_training = exists(actions)
+        is_sampling = not is_training
+
+        assert not (is_training and exists(style_token)), 'style token cannot be set during training'
+
+        # joint token
+
+        joint_tokens = self.joint_to_token(joint_state)
+        joint_tokens = rearrange(joint_tokens, 'b d -> b 1 d')
+
+        # take care of the needed style token during training
+
+        if is_training:
+            action_vae_tokens = self.action_to_vae_tokens(actions)
+
+            vae_input = cat((action_vae_tokens, joint_tokens), dim = 1)
+
+            vae_encoder_embed = self.vae_encoder(vae_input)
+
+            # cross attention pool
+
+            attn_pool_queries = repeat(self.attn_pool_query, 'd -> b 1 d', b = batch)
+
+            pooled_vae_embed = self.attn_pooler(attn_pool_queries, vae_encoder_embed)
+
+            style_mean, style_log_variance = self.to_style_vector_mean_log_variance(pooled_vae_embed)
+
+            # reparam
+
+            style_std = (0.5 * style_log_variance).exp()
+
+            noise = torch.randn_like(style_mean)
+
+            style_token = style_mean + style_std * noise
+
+        elif exists(style_token) and style_token.ndim == 1:
+
+            style_token = repeat(style_token, 'd -> b 1 d', b = batch)
+
+        else:
+            # or just zeros during inference, as in the paper
+
+            style_token = torch.zeros((batch, 1, self.dim), device = device)
+
+        # detr like encoder / decoder
+
+        encoder_input = cat((style_token, state_tokens, joint_tokens), dim = 1)
+
+        encoded = self.encoder(encoder_input)
+
+        decoder_input = repeat(self.action_queries, 'na d -> b na d', b = batch)
+
+        decoded = self.decoder(decoder_input, context = encoded)
+
+        pred_actions = self.decoder_embed_to_actions(decoded)
+
+        if not is_training:
+            return pred_actions
+
+        # take care of training loss
+
+        action_recon_loss = self.action_loss_fn(pred_actions, actions)
+
+        # my first time with this loss, so please open an issue if incorrect
+
+        vae_kl_loss = (
+            style_log_variance.exp().square()
+            + style_mean.square()
+            - (0.5 * style_log_variance)
+            - 1.
+        ).sum(dim = -1).mean()
+
+        loss_breakdown = (action_recon_loss, vae_kl_loss)
+
+        total_loss = (
+            action_recon_loss +
+            vae_kl_loss * self.vae_kl_loss_weight
+        )
+
+        if not return_loss_breakdown:
+            return total_loss
+
+        return total_loss, loss_breakdown
 
 # classes
 
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,4 +1,30 @@
+import torch
 import pytest
 
-def test_srt_h():
-    assert True
+@pytest.mark.parametrize('pass_custom_style', (False, True))
+def test_act(
+    pass_custom_style
+):
+    from SRT_H.SRT_H import ACT
+
+    act = ACT(
+        dim = 512,
+        dim_joint_state = 17,
+        action_chunk_len = 16
+    )
+
+    states = torch.randn(3, 512, 512)
+    joint_state = torch.randn(3, 17)
+
+    actions = torch.randn(3, 16, 20)
+
+    loss = act(states, joint_state, actions)
+    loss.backward()
+
+    # after a lot of data and training ...
+
+    style_token = torch.ones(512) if pass_custom_style else None
+
+    sampled_actions = act(states, joint_state, style_token = style_token) # (3, 16, 20)
+
+    assert sampled_actions.shape == (3, 16, 20)
